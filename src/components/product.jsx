18116c79dfebc0f33d143d8058a30e5e938f49c8
ll00lll00l0l <ll00lll00l0l@gmail.com>
2024-04-21 00:07:55 +0530

    update

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -5,6 +5,7 @@ import { IoIosAddCircle } from "react-icons/io";
 import { HashLink as Link } from "react-router-hash-link";
 
 const Product = () => {
+  const location = useLocation();
   const [selectedProductId, setSelectedProductId] = useState(0);
   const [cartItems, setCartItems] = useState([]);
 
@@ -26,30 +27,30 @@ const Product = () => {
             <div className="mainproduct">
               <img src={productsData[selectedProductId].imagePath} alt={productsData[selectedProductId].name} />
               <h2>{productsData[selectedProductId].name}</h2>
-              <p>
-                OMR ${productsData[selectedProductId].price}  
+              <p className="productprice">
+                <span>OMR ${productsData[selectedProductId].price}  </span>
                 <del>${productsData[selectedProductId].price}</del>
               </p>
               <button onClick={() => addToCart(selectedProductId)} className="prodcartbutton">ADD CART</button>
             </div>
           )}
         </div>
-        <div className="productsub">
+        <div className="productsub " >
           {productsData.map((product, index) => (
-            <div className="itemprod" key={product.id}>
+            <div className="itemprod" key={product.id} style={{ border: selectedProductId === index ? " 2px solid#1AA5C3" : " 1px solid#B9B9B9" }} >
               <div className="iteamstyles">
-                <span><img src={product.imagePath} alt={product.name} width={100} height={130} />
+                <div><img src={product.imagePath} alt={product.name} width={100} height={130} />
                 <Link
                   to="#product"
                   smooth
                   className="showbuttom"
                   onClick={() => handleShowDescription(index)}
                 >
-                  <IoIosAddCircle size={40} style={{ color: selectedProductId === index ? "#1AA5C3" : "#B9B9B9" }} />
-                </Link></span>
+                  <IoIosAddCircle size={25} style={{ color: selectedProductId === index ? "#1AA5C3" : "#B9B9B9" }} />
+                </Link></div>
                 <h2>{product.name}</h2>
-                <p>
-                  OMR ${product.price} <del>${product.price}</del>
+                <p className="productprice">
+                  OMR <span>${product.price}</span><del>${product.price}</del>
                 </p>
               </div>
             </div>
